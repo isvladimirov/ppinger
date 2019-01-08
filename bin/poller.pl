19d396f87b1d472e90d802eff4fe0cc075e3dcdf
duk3L3t0 <luiseal.mail@gmail.com>
2019-01-08 22:07:02 +1000

    Add debug to poller.

--- bin/poller.pl
+++ bin/poller.pl
@@ -9,9 +9,11 @@
 
 use lib "../include";
 use PMySQL;
+use Config::IniFiles;
 use strict;
 use constant
 {
+    DEBUG => 1, # '0' - Debug is OFF, '1' - Debug is ON
     STATUS_ALL => 0,
     STATUS_DOWN => 1,
     STATUS_ALIVE => 2,
@@ -19,8 +21,10 @@ use constant
     STATUS_DISABLED => 4,
 };
 
+!DEBUG or print "Loading settings...\n";
 my $config = Config::IniFiles->new( -file => "../etc/ppinger.cfg" );
-# Try to open database
+
+!DEBUG or print "Try to open database...\n";
 my $db = PMySQL->new(
     $config->val('SQL', 'db_host'),
     $config->val('SQL', 'db_user'),
@@ -28,12 +32,16 @@ my $db = PMySQL->new(
     $config->val('SQL', 'db_name')
 );
 
+!DEBUG or print "Polling downed hosts...\n";
+!DEBUG or print "Polling unknown hosts...\n";
+!DEBUG or print "Polling alive hosts...\n";
 #
 # TODO: Here will be main code of a poller
 #
 
-# Close database
+!DEBUG or print "Closing database...\n";
 $db->DESTROY();
 
+!DEBUG or print "Exit.\n";
 # End of main function
 1;
